254b7f60780722e02264c7ffc381242546884f0c
Bernardo20arantes <bernardo20arantes@gmail.com>
2024-06-08 16:43:38 -0300

    Configurando o projeto Json Server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 // Serve static files from the "assets" directory
 app.use(express.static(path.join(__dirname, 'assets')));
@@ -8,6 +9,18 @@ app.use(express.static(path.join(__dirname, 'assets')));
 // Define a route for the homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+// Define a route to serve the JSON data
+app.get('/data', (req, res) => {
+  fs.readFile(path.join(__dirname, 'data' ,'db.json'), 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading data file');
+      return;
+    }
+    res.header('Content-Type', 'application/json');
+    res.send(data);
+  });
 });
 
 // Start the server
